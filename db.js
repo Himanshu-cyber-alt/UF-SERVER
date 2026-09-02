f671e6b4f2aaa50282f6e4734ea62ee32d56201b
Himanshu Pagare <155243085+Himanshu-cyber-alt@users.noreply.github.com>
2026-09-02 22:33:45 +0530

    yes

--- db.js
+++ db.js
@@ -3,8 +3,6 @@ import { Pool } from "pg";
 
 dotenv.config();
 
-
-
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
---
+++ test.js
@@ -0,0 +1,7 @@
+
+let cnt = 0;
+while(true){
+cnt++;
+console.log(cnt);
+
+}
